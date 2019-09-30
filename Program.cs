018aa873b384ba76c4cc1fcc8e730b24df8af4f0
Placide IRANDORA <placideirandora@gmail.com>
2019-09-30 23:12:52 +0200

    Tell the user an adventure and ask them what they can do about it

--- Program.cs
+++ Program.cs
@@ -20,7 +20,18 @@ namespace text_adventure_game
 
         public static void firstSection()
         {
+            string choice;
 
+            Console.WriteLine("You Wake Up In Class With An Angry Mr. Storm Standing Over You.");
+            Console.WriteLine("What Do You Do?");
+            Console.WriteLine("1. Punch Him.");
+            Console.WriteLine("2. Cry.");
+            Console.WriteLine("3. Pee A Little. ");
+            Console.WriteLine("Choice: ");
+
+            choice = Console.ReadLine().ToLower();
+
+            Console.Clear();
         }
 
         public static void secondSection()
