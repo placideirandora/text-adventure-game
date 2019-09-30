dcd6ad713a3944ed10a336cf20dbb3361ce11f0a
Placide IRANDORA <placideirandora@gmail.com>
2019-09-30 22:14:16 +0200

    Add the game's initial start point

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace text_adventure_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The game is loading...!");
+        }
+    }
+}
